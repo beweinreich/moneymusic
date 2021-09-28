c2c2ae184f89652e591fe67a15058a414cf5dc01
Brian Weinreich <be.weinreich@gmail.com>
2021-09-27 20:49:15 -0400

    wave form colors

--- pages/index.js
+++ pages/index.js
@@ -93,68 +93,71 @@ export default function Home() {
     let noteLetter;
     let octave = 4;
     let arping = true;
-
     let ticks = 0;
-    Tone.loaded().then(() => {
-      const loop = new Tone.Loop((time) => {
-        ticks++;
 
-        noteLetter = getNextNote(noteLetter);
-        let note = noteLetter + octave;
+    const wave = new Tone.Waveform();
+    Tone.Master.connect(wave);
 
-        const oneFour = [0, 3][randomInt(2)];
-        let chord = Chord.get(scale[oneFour]);
-        let chordNotes = chord.notes;
-        let chordNotesPianoBass = chord.notes.map(
-          (note) => `${note}${octave - 1}`
-        );
-        let chordNotesPiano = chord.notes.map((note) => `${note}${octave}`);
+    const loop = new Tone.Loop((time) => {
+      ticks++;
 
-        const playNote = randomInt(10) > 2;
+      noteLetter = getNextNote(noteLetter);
+      let note = noteLetter + octave;
 
-        if (ticks % 40 === 0) {
-          arping = !arping;
+      Tone.Draw.schedule(() => drawWaveform(wave, 600, 200), time);
+
+      const oneFour = [0, 3][randomInt(2)];
+      let chord = Chord.get(scale[oneFour]);
+      let chordNotes = chord.notes;
+      let chordNotesPianoBass = chord.notes.map(
+        (note) => `${note}${octave - 1}`
+      );
+      let chordNotesPiano = chord.notes.map((note) => `${note}${octave}`);
+
+      const playNote = randomInt(10) > 2;
+
+      if (ticks % 40 === 0) {
+        arping = !arping;
+      }
+
+      if (arping) {
+        // some arping on the chords
+        if (ticks % 4 === 0) {
+          chordNotesPianoBass.map((note, idx) => {
+            const timeOffset = idx === 0 ? 0 : Tone.Time(`${idx * 16}n`);
+            piano.triggerAttackRelease(note, "8n", time + timeOffset);
+          });
         }
-
-        if (arping) {
-          // some arping on the chords
-          if (ticks % 4 === 0) {
-            chordNotesPianoBass.map((note, idx) => {
-              const timeOffset = idx === 0 ? 0 : Tone.Time(`${idx * 16}n`);
-              piano.triggerAttackRelease(note, "8n", time + timeOffset);
-            });
+      } else {
+        // chords together
+        if (ticks % 8 === 0) {
+          if (randomInt(10) > 2) {
+            piano.triggerAttackRelease(chordNotesPianoBass, "1n", time);
           }
         } else {
-          // chords together
-          if (ticks % 8 === 0) {
-            if (randomInt(10) > 2) {
-              piano.triggerAttackRelease(chordNotesPianoBass, "1n", time);
-            }
-          } else {
-            if (playNote && randomInt(10) > 8) {
-              piano.triggerAttackRelease(chordNotesPianoBass, "1n", time);
-            }
+          if (playNote && randomInt(10) > 8) {
+            piano.triggerAttackRelease(chordNotesPianoBass, "1n", time);
           }
         }
+      }
 
-        if (arping) {
-          if (ticks % 4 && playNote) {
-            // if we are "arpeggiating" pick one of the notes from the chord
-            // to avoid dissonance
-            note = chordNotesPiano[randomInt(chord.notes.length)];
-            piano.triggerAttackRelease(note, "8n", time);
-          }
-        } else {
-          if (playNote) {
-            piano.triggerAttackRelease(
-              chordNotesPiano[randomInt(chordNotesPiano.length)],
-              durations[randomInt(durations.length)],
-              time
-            );
-          }
+      if (arping) {
+        if (ticks % 4 && playNote) {
+          // if we are "arpeggiating" pick one of the notes from the chord
+          // to avoid dissonance
+          note = chordNotesPiano[randomInt(chord.notes.length)];
+          piano.triggerAttackRelease(note, "8n", time);
         }
-      }, "32n").start();
-    });
+      } else {
+        if (playNote) {
+          piano.triggerAttackRelease(
+            chordNotesPiano[randomInt(chordNotesPiano.length)],
+            durations[randomInt(durations.length)],
+            time
+          );
+        }
+      }
+    }, "32n").start();
   };
 
   useEffect(() => {
@@ -212,7 +215,13 @@ export default function Home() {
       <button onClick={pausePlayback}>Stop</button>
       <p />
       <p>In the key of {keyLetter}</p>
-      <div
+      <canvas
+        id="canvas"
+        width="600"
+        height="200"
+        style={{ backgroundColor: "black", borderRadius: 20 }}
+      ></canvas>
+      {/*<div
         dangerouslySetInnerHTML={{ __html: playingText }}
         style={{
           overflow: "scroll",
@@ -223,7 +232,57 @@ export default function Home() {
           borderRadius: 20,
           padding: 20,
         }}
-      />
+      />*/}
     </div>
   );
 }
+
+function drawWaveform(wave, w, h) {
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+
+  let buffer = wave.getValue(0);
+
+  let start = 0;
+  for (let i = 1; i < buffer.length; i++) {
+    if (buffer[i - 1] < 0 && buffer[i] >= 0) {
+      start = i;
+      break;
+    }
+  }
+
+  let end = start + buffer.length / 2;
+
+  const color = randomColor();
+  for (let i = start; i < end; i++) {
+    let x1 = map(i - 1, start, end, 0, w);
+    let y1 = map(buffer[i - 1], -1, 1, 0, h);
+    let x2 = map(i, start, end, 0, w);
+    let y2 = map(buffer[i], -1, 1, 0, h);
+
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.strokeStyle = color;
+    ctx.stroke();
+  }
+}
+
+function map(n, start1, stop1, start2, stop2, withinBounds) {
+  const newval = ((n - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
+  if (!withinBounds) {
+    return newval;
+  }
+  if (start2 < stop2) {
+    return constrain(newval, start2, stop2);
+  } else {
+    return constrain(newval, stop2, start2);
+  }
+}
+
+function constrain(n, low, high) {
+  return Math.max(Math.min(n, high), low);
+}
+
+const randomColor = () =>
+  `#${Math.floor(Math.random() * 16777215).toString(16)}`;
